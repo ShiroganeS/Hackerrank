fb0259840f65ac01bc5d4c5102d6a312a6310ec0
Emil <emil.wozniak.591986@gmail.com>
2022-05-10 20:15:35 +0200

    Update condition.py
    Remove comments

--- condition.py
+++ condition.py
@@ -6,12 +6,6 @@ import random
 import re
 import sys
 
-# Given an integer, , perform the following conditional actions:
-# If  is odd, print Weird
-# If  is even and in the inclusive range of 2 to 5, print Not Weird
-# If  is even and in the inclusive range of 6 to 20, print Weird
-# If  is even and greater than 20 , print Not Weird
-
 if __name__ == '__main__':
   if __name__ == '__main__':
     n = int(raw_input().strip())
